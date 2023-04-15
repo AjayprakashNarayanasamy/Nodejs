e78ba0207536467a04719a6494847838a7574d06
AjayPrakash06 <ajay.prakash@agilisium.com>
2023-04-15 20:20:24 +0530

    Changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const customer = require("./routes/shop");
 const path = require("path");
 const rootdir = require("./utils/path");
 
-const app = express();
+const app = express;
 
 app.use(bodyParser());
 app.use(express.static(path.join(__dirname, "/public")));
