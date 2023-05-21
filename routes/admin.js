1fc9ee2a0262d0d1e707d153be62861432c1e70d
AjayPrakash06 <ajay.prakash@agilisium.com>
2023-05-21 10:47:19 +0530

    All commit before pug

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const path = require("path");
-const rootdir = require("../utils/path")
+const rootdir = require("../utils/path");
+const { Module } = require("module");
 
 // console.log("express" , express)
 // console.log("express" , express())
 const router = express.Router();
+const arr = []
 
 router.get("/add-product", (req, res) => {
   console.log("Text");
   res.sendFile(path.join(rootdir, "views", "add-product.html"));
 });
 router.post("/add-product", (req, res) => {
-  console.log(req.body);
+  
+  arr.push(req.body)
+  console.log(arr)
   res.redirect("/");
 });
-module.exports = router;
+exports.routes = router;
+exports.product = arr;
