ec338a52832dd161f5dd77d9d99224264af9301a
AjayPrakash06 <ajay.prakash@agilisium.com>
2023-06-19 02:21:31 +0530

    Dynamic Routes

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,21 @@
 const Products = require("../models/product");
+const CartClass = require("../models/cart");
 exports.index = (req, res) => {
-  res.render("shop/index", {
-    Heading: "Shop",
-    active: "Index",
+  Products.fetchProduct((product) => {
+    res.render("shop/index", {
+      prod: product,
+      Heading: "Shop",
+      active: "productList",
+    });
+  });
+};
+exports.productDescription = (req, res) => {
+  Products.fetchProductById(req.params.productId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      Heading: "Product Description",
+      active: null,
+    });
   });
 };
 exports.showProduct = (req, res) => {
@@ -13,13 +26,42 @@ exports.showProduct = (req, res) => {
       active: "view-product",
     });
   });
-  
+};
+exports.postCart = (req, res) => {
+  Products.fetchProductById(req.body.productId, (product) => {
+
+    CartClass.addCart(req.body.productId, product.cost);
+    res.redirect("/");
+  });
 };
 exports.cart = (req, res) => {
-  res.render("shop/cart", {
-    Heading: "Cart",
-    active: "cart",
+  CartClass.displayCart((productInCartId) => {
+    Products.fetchProduct((availableProducts) => {
+      const cartProducts = [];
+      for (let product of availableProducts) {
+        let quantity = productInCartId.product.find(
+          (val) => val.id === product.id
+        );
+        if (quantity) {
+          cartProducts.push({ productData: product, qty: quantity.qty });
+        }
+      }
+   
+      res.render("shop/cart", {
+        Heading: "Cart",
+        active: "cart",
+        products: cartProducts,
+      });
+    });
   });
+};
+exports.deleteCartItem = (req, res) => {
+
+  Products.fetchProductById(req.body.id, (product) => {
+    
+    CartClass.deleteCart(req.body.id, product.cost);
+  });
+  res.redirect("/")
 };
 exports.checkout = (req, res) => {
   res.render("shop/checkout", {
@@ -27,11 +69,9 @@ exports.checkout = (req, res) => {
     active: "checkout",
   });
 };
-exports.orders = (req , res) =>{
-  res.render("shop/orders",{
-    Heading:"Orders",
-    active:"orders"
-  })
-
-}
-
+exports.orders = (req, res) => {
+  res.render("shop/orders", {
+    Heading: "Orders",
+    active: "orders",
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const Cart = require("./cart");
 const filePath = path.join(
   path.dirname(process.mainModule.filename),
   "data",
@@ -15,23 +16,59 @@ const getProductFromFile = (cb) => {
   });
 };
 module.exports = class Product {
-  constructor(title) {
-   
-    this.title = title;
-    this.image = image;
-    this.description = description;
-    this.cost = cost
+  constructor(product) {
+    this.product = product;
   }
   save() {
-    getProductFromFile((product) => {
-      console.log(product, "product");
-      product.push(this.title);
-      fs.writeFile(filePath, JSON.stringify(product), (err) => {});
+    getProductFromFile((products) => {
+      if (this.product.id.length === 0) {
+    
+        this.product = { ...this.product, id: Math.random().toString() };
+        products.push(this.product);
+        fs.writeFile(filePath, JSON.stringify(products), (err) => {});
+      } else {
+       
+        
+
+        let existingProductId = products.findIndex(
+          (existingProduct) => existingProduct.id === this.product.id
+        );
+     
+
+        products[existingProductId] = this.product;
+
+        fs.writeFile(filePath, JSON.stringify(products), (err) => {});
+      }
     });
 
     // products.push(this.title);
   }
+  static delete(id) {
+    getProductFromFile((product) => {
+  
+      const productCost = product.find((val) => {
+        return val.id === id;
+      });
+      const value = product.filter((val) => {
+  
+
+        return val.id !== id;
+      });
+    
+      fs.writeFile(filePath, JSON.stringify(value), (err) => {
+        if (!err) {
+          Cart.deleteCart(id, productCost.cost);
+        }
+      });
+    });
+  }
+
   static fetchProduct(cb) {
     getProductFromFile(cb);
+  }
+  static fetchProductById(id, cb) {
+    getProductFromFile((product) => {
+      cb(product.find((val) => val.id === id));
+    });
   }
 };
