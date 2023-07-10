c9c4b66dc0794497ef1377b9c636d1ad420a8183
AjayPrakash06 <ajay.prakash@agilisium.com>
2023-07-10 10:26:35 +0530

    Internal Changes

--- controllers/404error.js
+++ controllers/404error.js
@@ -1,5 +1,5 @@
 exports.error = (req, res) => {
   res
     .status(400)
-    .render("404", { Heading: "No Data Available", active: "false" });
+    .render("404", { Heading: "No ata Available", active: "false" });
 };
